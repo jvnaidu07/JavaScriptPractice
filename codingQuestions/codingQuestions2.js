5d7fade248f99d05bf68a2fd6695eb38b594c0a6
jvnaidu07 <jvnaidu07@gmail.com>
2023-05-11 20:48:39 +0530

    save changes

--- codingQuestions/codingQuestions2.js
+++ codingQuestions/codingQuestions2.js
@@ -17,5 +17,37 @@ for (var i=0; i<len; i++) {
     }
 }
 console.log(sendmax)
+// remove duplicate elements from string
+let str = "mango banana mango apple orange apple grapes grapes orange"
+let arr1 = str.split(" ")
 
+let unique = [];
+
+for( var i=0; i<arr1.length; i++) {
+    if(unique.indexOf(arr1[i]) == -1) {
+        unique.push(arr1[i])
+    }
+}
+console.log(unique)
+
+let str1 = "mango banana mango apple grapes orange"
+let arr2 = str1.split(" ")
+let lenn = arr2.length;
+let repeated = {};
+
+for(var i=0; i<lenn; i++) {
+    let word = arr2[i]
+    if(repeated[word] === undefined) {
+        repeated[word] = 1
+    }
+    else {
+        repeated[word]++;
+    }
+}
+
+for(let word in repeated) {
+    if(repeated[word] > 1) {
+        console.log(word)
+    }
+}
 
