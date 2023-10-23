2da94c8f9e590861b8c22348ffb07487fc611616
jvnaidu07 <jvnaidu07@gmail.com>
2023-10-23 17:25:01 +0530

    save

--- codingQuestions/codingQuestions2.js
+++ codingQuestions/codingQuestions2.js
@@ -1,18 +1,18 @@
 //JS coding questions
 // find secondLargest number from array
-var arr = [1,2,3,62,4,5,6,1,8,9,7,5,42,12,3,2]
+var arr = [1, 2, 3, 62, 4, 5, 6, 1, 8, 9, 7, 5, 42, 12, 3, 2]
 var len = arr.length;
 
 var max = -Infinity
 var sendmax = -Infinity
 
-for (var i=0; i<len; i++) {
+for (var i = 0; i < len; i++) {
     max = Math.max(max, arr[i])
 }
 console.log(max)
 
-for (var i=0; i<len; i++) {
-    if(arr[i] < max) {
+for (var i = 0; i < len; i++) {
+    if (arr[i] < max) {
         sendmax = Math.max(sendmax, arr[i])
     }
 }
@@ -23,8 +23,8 @@ let arr1 = str.split(" ")
 
 let unique = [];
 
-for( var i=0; i<arr1.length; i++) {
-    if(unique.indexOf(arr1[i]) == -1) {
+for (var i = 0; i < arr1.length; i++) {
+    if (unique.indexOf(arr1[i]) == -1) {
         unique.push(arr1[i])
     }
 }
@@ -36,9 +36,9 @@ let arr2 = str1.split(" ")
 let lenn = arr2.length;
 let repeated = {};
 
-for(var i=0; i<lenn; i++) {
+for (var i = 0; i < lenn; i++) {
     let word = arr2[i]
-    if(repeated[word] === undefined) {
+    if (repeated[word] === undefined) {
         repeated[word] = 1
     }
     else {
@@ -46,19 +46,19 @@ for(var i=0; i<lenn; i++) {
     }
 }
 
-for(let word in repeated) {
-    if(repeated[word] > 1) {
+for (let word in repeated) {
+    if (repeated[word] > 1) {
         console.log(word)
     }
 }
 
 // find each character length
-var input = ["a","a","b","b","b","c","c","c","c"]
+var input = ["a", "a", "b", "b", "b", "c", "c", "c", "c"]
 var numbeChar = {};
 
-for(var i=0;i<input.length; i++) {
+for (var i = 0; i < input.length; i++) {
     let word5 = input[i]
-    if(numbeChar[word5] === undefined) {
+    if (numbeChar[word5] === undefined) {
         numbeChar[word5] = 1
     }
     else {
